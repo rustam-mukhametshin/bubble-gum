5fe05551de1fc86d0a66a986a8217138aa5be370
iproman <itechman@yandex.ru>
2021-06-05 18:43:17 +0500

    Answers

---
+++ src/removeFirstAndLastChar/x_answer.js
@@ -0,0 +1,12 @@
+/*
+const arr = [...str] // .split('')
+arr.shift();
+arr.pop();
+return arr.join('');
+*/
+
+// return str.substring(1, str.length - 1)
+
+// return str.slice(1, -1);
+
+// return str.replace(/^\w|\w$/g, '');
